8cf7e746e97fe1f52686dc95fe9198b3c79b9e21
Khoa Nguyễn <khoaruoi69@gmail.com>
2022-10-03 16:47:21 +0700

    khoi tao doi tuong trong {} AuthUser

--- react-typescript-demo/src/Components/state/User.tsx
+++ react-typescript-demo/src/Components/state/User.tsx
@@ -5,7 +5,7 @@ type AuthUser = {
     email: string
 }
 export const User =()=>{
-    const [user, setUser]= useState<AuthUser|null>(null)
+    const [user, setUser]= useState<AuthUser>({}as AuthUser) // khi tao doi tuong trong loaij AuthUser
     const handleLogin = () => {
         setUser({
             name: "khoa",
@@ -13,14 +13,14 @@ export const User =()=>{
         })
     }
     const handleLogout = () => {
-        setUser(null)
+        setUser({} as AuthUser)
     }
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
             <button onClick={handleLogout}>Logout</button>
-            <div> User name is {user?.name} </div>
-            <div> User email is {user?.email} </div>
+            <div> User name is {user.name} </div>
+            <div> User email is {user.email} </div>
         </div>
     )
 }
