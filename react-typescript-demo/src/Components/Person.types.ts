91731545323ee70006403bfa27e3663ae11c28a7
Khoa Nguyễn <khoaruoi69@gmail.com>
2022-10-03 16:14:45 +0700

    bai 8 _ file.types.ts

---
+++ react-typescript-demo/src/Components/Person.types.ts
@@ -0,0 +1,9 @@
+import { type } from "os";
+
+export type Name = {
+  first: string;
+  last: string;
+};
+export type PersonProps = {
+  name: Name;
+};
---
+++ react-typescript-demo/src/Components/state/Loggedln.tsx
@@ -0,0 +1,9 @@
+export const LoggedIn = ()=>{
+    const handleLogin = ()=>{}
+    const handeLogout = ()=>{}
+    return (
+        <div>
+            <button>Login</button>
+        </div>
+    )
+}
